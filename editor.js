91243bfe159724ca12e18ac69307af16b071bd0b
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-12-10 20:31:22 +0100

    Update editor.js
    -Added variable set.

--- editor.js
+++ editor.js
@@ -1,4 +1,7 @@
 function run() {
-
+  let htmlCode = document.getElementById("html-code").value;
+  let cssCode = document.getElementById("css-code").value;
+  let jsCode = document.getElementById("javascript-code").value;
+  let output = document.getElementById("output");
 
 }
